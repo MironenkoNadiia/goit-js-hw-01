0635be7111aec1382aa5883fcefe8a6e64de8491
MironenkoNadiia <65623154+MironenkoNadiia@users.noreply.github.com>
2020-08-14 14:58:24 +0300

    Add files via upload

---
+++ js/task1.js.js
@@ -0,0 +1,10 @@
+const name = "«Генератор защитного поля»";
+
+let price = 1000;
+const firstMessage = `Выбран ${name},цена за штуку ${price} кредитов`;
+console.log(firstMessage);
+
+let price = 2000;
+const secondMessage = `Выбран ${name},цена за штуку ${price} кредитов`;
+console.log(secondMessage);
+
